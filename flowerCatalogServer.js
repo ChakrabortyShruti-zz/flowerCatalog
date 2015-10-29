eba5b851b651e54e682aff124ed770340c86b470
shrutic <shrutic@INshrutic.local>
2015-10-29 12:48:05 +0530

    removed console.log

--- flowerCatalogServer.js
+++ flowerCatalogServer.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 
 var flowerCatalog = fs.readFileSync('./homePage.html','UTF-8');
 var listener = function(req,res) {
+	console.log("------",res.url);
+	console.log(res.statusCode);
 	res.write(flowerCatalog);
 	res.end();
 };
