99207b3d37b1c63120d615ec39bc43aafb0b67c0
shrutic <shrutic@INshrutic.local>
2015-10-30 10:09:04 +0530

    to start the server

--- flowerCatalogServer.js
+++ flowerCatalogServer.js
@@ -1,13 +1,1 @@
 var http = require('http');
-var fs = require('fs');
-
-var flowerCatalog = fs.readFileSync('./homePage.html','UTF-8');
-var listener = function(req,res) {
-	console.log("------",res.url);
-	console.log(res.statusCode);
-	res.write(flowerCatalog);
-	res.end();
-};
-
-var server = http.createServer(listener);
-server.listen(4000);
